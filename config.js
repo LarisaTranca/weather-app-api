e5472ebfd689c703d62004598b3da6ac33f8a579
LarisaTranca <larisa.tranca15@gmail.com>
2018-05-06 23:17:36 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '963549583810977',
+  clientSecret: '93d5f801bea47ec39532914b578d025a',
+  callbackURL: 'https://e53e164f.ngrok.io/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '568404380953-qc6esd5drvc5n07j49chi52h7fs72p4t.apps.googleusercontent.com',
+  clientSecret: 'QGHAmyqYkrYjJWq4Tn2vd3Ts',
+  callbackURL: 'https://e53e164f.ngrok.io/auth/google/callback',
+};
---
+++ library/connection.js
@@ -0,0 +1,11 @@
+var db = {
+    user: 'rootuser',
+    password: 'overdrive1!',
+    server: 'weather-ml.database.windows.net',
+    database: 'weather',
+    options: {
+        encrypt: 'true'
+    }
+}
+
+module.exports = db;
